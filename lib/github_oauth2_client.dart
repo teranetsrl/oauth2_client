d6a57739b9e033249c1bc9ca971ba2206f915ffb
okrad <enrico.triolo@gmail.com>
2020-04-02 16:38:40 +0200

    Update GitHub client

--- lib/github_oauth2_client.dart
+++ lib/github_oauth2_client.dart
@@ -1,12 +1,9 @@
 import 'package:oauth2_client/oauth2_client.dart';
 import 'package:meta/meta.dart';
 
-/// Implements an OAuth2 client that uses Google services to authorize requests.
+/// Implements an OAuth2 client against GitHub
 ///
-/// In order to use this client you need to first configure OAuth2 credentials in the Google API console (https://console.developers.google.com/apis/)
-/// First you need to create a new Project if it doesn't already exists, then you need to create the OAuth2 credentials ("OAuth Client ID").
-/// Select iOS as Application Type, specify a name for the client and in the "Bundle ID" field insert your custom uri scheme
-/// (for example 'com.example.app', but you can use whatever uri scheme you want).
+/// In order to use this client you need to first create a new OAuth2 App in the GittHub Developer Settings (https://github.com/settings/developers)
 ///
 class GitHubOAuth2Client extends OAuth2Client {
   GitHubOAuth2Client(
