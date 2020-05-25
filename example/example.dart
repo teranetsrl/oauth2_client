a96155d1fd0bacce99adb45861ad400937ff5ad2
Brett Morgan <brett.morgan@gmail.com>
2020-05-25 17:32:30 +1000

    Add pedantic and update dependencies

--- example/example.dart
+++ example/example.dart
@@ -1,12 +1,11 @@
 import 'package:oauth2_client/oauth2_helper.dart';
 import 'package:oauth2_client/google_oauth2_client.dart';
-import 'package:http/http.dart' as http;
 
 class Oauth2ClientExample {
   Oauth2ClientExample();
 
   Future<void> fetchFiles() async {
-    OAuth2Helper hlp = OAuth2Helper(GoogleOAuth2Client(
+    var hlp = OAuth2Helper(GoogleOAuth2Client(
         redirectUri: 'com.teranet.app:/oauth2redirect',
         customUriScheme: 'com.teranet.app'));
 
@@ -15,7 +14,7 @@ class Oauth2ClientExample {
         clientId: 'XXX-XXX-XXX',
         scopes: ['https://www.googleapis.com/auth/drive.readonly']);
 
-    http.Response resp =
+    var resp =
         await hlp.get('https://www.googleapis.com/drive/v3/files');
 
     print(resp.body);
