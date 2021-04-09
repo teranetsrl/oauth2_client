8800869423823b6bf7644aab5f31ea3027991379
okrad <enrico.triolo@gmail.com>
2021-04-09 21:56:42 +0200

    Cleanup response code

--- lib/access_token_response.dart
+++ lib/access_token_response.dart
@@ -7,14 +7,6 @@ import 'package:oauth2_client/oauth2_response.dart';
 /// see https://tools.ietf.org/html/rfc6749#section-5.2
 
 class AccessTokenResponse extends OAuth2Response {
-  // String? accessToken;
-  // String? tokenType;
-  // int? expiresIn;
-  // String? refreshToken;
-  // List<String>? scope;
-
-  // DateTime? expirationDate;
-
   AccessTokenResponse() : super();
   AccessTokenResponse.errorResponse() : super.errorResponse();
 
@@ -25,16 +17,11 @@ class AccessTokenResponse extends OAuth2Response {
       {List<String>? requestedScopes}) {
     AccessTokenResponse resp;
 
-    var defMap = {
-      'http_status_code': response.statusCode,
-      // 'resp_ts': DateTime.now()
-    };
-    // print('SON QUAAAAAAAAAAAAAAAA');
+    var defMap = {'http_status_code': response.statusCode};
     if (response.body != '') {
       Map<String, dynamic> rMap = jsonDecode(response.body);
       //From Section 4.2.2. (Access Token Response) of OAuth2 rfc, the "scope" parameter in the Access Token Response is
       //"OPTIONAL, if identical to the scope requested by the client; otherwise, REQUIRED."
-
       if ((!rMap.containsKey('scope') ||
           rMap['scope'] == null ||
           rMap['scope'].isEmpty)) {
@@ -60,8 +47,9 @@ class AccessTokenResponse extends OAuth2Response {
 
         rMap['expires_in'] = expiresIn;
 
-        rMap['expiration_date'] =
-            DateTime.now().add(Duration(seconds: expiresIn!));
+        rMap['expiration_date'] = DateTime.now()
+            .add(Duration(seconds: expiresIn!))
+            .millisecondsSinceEpoch;
       }
 
       resp = AccessTokenResponse.fromMap({...rMap, ...defMap});
@@ -73,135 +61,11 @@ class AccessTokenResponse extends OAuth2Response {
     }
 
     return resp;
-/*
-    AccessTokenResponse resp;
-
-    if (response.statusCode != 404) {
-      Map<String, dynamic> respMap = jsonDecode(response.body);
-      //From Section 4.2.2. (Access Token Response) of OAuth2 rfc, the "scope" parameter in the Access Token Response is
-      //"OPTIONAL, if identical to the scope requested by the client; otherwise, REQUIRED."
-      if ((!respMap.containsKey('scope') ||
-              respMap['scope'] == null ||
-              respMap['scope'].isEmpty) &&
-          requestedScopes != null) {
-        respMap['scope'] = requestedScopes;
-      }
-      respMap['http_status_code'] = response.statusCode;
-
-      resp = AccessTokenResponse.fromMap(respMap);
-    } else {
-      resp = AccessTokenResponse();
-    }
-
-    return resp;
-*/
   }
 
-/*
-   {
-
-    if (isValid()) {
-      accessToken = map['access_token'];
-      //Some providers (e.g. Slack) don't return the token_type parameter, even if it's required...
-      //In those cases, fallback to "bearer"
-      tokenType = map['token_type'] ?? 'Bearer';
-      if (map.containsKey('refresh_token')) refreshToken = map['refresh_token'];
-
-      if (map.containsKey('scope')) {
-        if (map['scope'] is List) {
-          List scopesJson = map['scope'];
-          scope = List.from(scopesJson);
-        } else {
-          //The OAuth 2 standard suggests that the scopes should be a space-separated list,
-          //but some providers (i.e. GitHub) return a comma-separated list
-          scope = map['scope']?.split(RegExp(r'[\s,]'));
-        }
-
-        scope = scope?.map((s) => s.trim()).toList();
-      }
-
-      if (map.containsKey('expires_in')) {
-        try {
-          expiresIn = map['expires_in'] is String
-              ? int.parse(map['expires_in'])
-              : map['expires_in'];
-        } on FormatException {
-          //Provide a fallback value if the expires_in parameter is not an integer...
-          expiresIn = 60;
-          //...But rethrow the exception!
-          rethrow;
-        }
-      }
-
-      expirationDate = null;
-
-      if (map.containsKey('expiration_date') &&
-          map['expiration_date'] != null) {
-        expirationDate =
-            DateTime.fromMillisecondsSinceEpoch(map['expiration_date']);
-      } else {
-        if (expiresIn != null) {
-          var now = DateTime.now();
-          expirationDate = now.add(Duration(seconds: expiresIn!));
-        }
-      }
-    }
-  }
-*/
-/*
-  factory AccessTokenResponse.fromHttpResponse(http.Response response,
-      {requestedScopes}) {
-    AccessTokenResponse resp;
-
-    if (response.statusCode != 404) {
-      Map<String, dynamic> respMap = jsonDecode(response.body);
-      //From Section 4.2.2. (Access Token Response) of OAuth2 rfc, the "scope" parameter in the Access Token Response is
-      //"OPTIONAL, if identical to the scope requested by the client; otherwise, REQUIRED."
-      if ((!respMap.containsKey('scope') ||
-              respMap['scope'] == null ||
-              respMap['scope'].isEmpty) &&
-          requestedScopes != null) {
-        respMap['scope'] = requestedScopes;
-      }
-      respMap['http_status_code'] = response.statusCode;
-
-      resp = AccessTokenResponse.fromMap(respMap);
-    } else {
-      resp = AccessTokenResponse();
-      resp.httpStatusCode = response.statusCode;
-    }
-
-    return resp;
-  }
-*/
   @override
   Map<String, dynamic> toMap() {
     return respMap;
-/*
-    var now = DateTime.now();
-
-    return {
-      ...respMap,
-      ...{
-        'expires_in': expirationDate?.difference(now).inSeconds,
-        'expiration_date': expirationDate?.millisecondsSinceEpoch,
-      }
-    };
-*/
-/*
-    return {
-      'http_status_code': httpStatusCode,
-      'access_token': accessToken,
-      'token_type': tokenType,
-      'refresh_token': refreshToken,
-      'scope': scope,
-      'expires_in': expirationDate?.difference(now).inSeconds,
-      'expiration_date': expirationDate?.millisecondsSinceEpoch,
-      'error': error,
-      'errorDescription': errorDescription,
-      'errorUri': errorUri
-    };
-*/
   }
 
   ///Checks if the access token is expired
@@ -210,8 +74,6 @@ class AccessTokenResponse extends OAuth2Response {
 
     if (expirationDate != null) {
       var now = DateTime.now();
-      // print('${expirationDate!.minute}:${expirationDate!.second}');
-      // print('${now.minute}:${now.second}');
       expired = expirationDate!.difference(now).inSeconds < 0;
     }
 
@@ -255,25 +117,15 @@ class AccessTokenResponse extends OAuth2Response {
     return isValid() ? respMap['refresh_token'] : null;
   }
 
+  set refreshToken(String? _tkn) {
+    respMap['refresh_token'] = _tkn;
+  }
+
   List<String>? get scope {
     var scopes;
 
     if (isValid()) {
       scopes = respMap['scope'];
-/*
-      if (respMap.containsKey('scope')) {
-        if (respMap['scope'] is List) {
-          List scopesJson = respMap['scope'];
-          scopes = List.from(scopesJson);
-        } else {
-          //The OAuth 2 standard suggests that the scopes should be a space-separated list,
-          //but some providers (i.e. GitHub) return a comma-separated list
-          scopes = respMap['scope']?.split(RegExp(r'[\s,]'));
-        }
-
-        scopes = scopes?.map((s) => s.trim()).toList();
-      }
-*/
     }
 
     return scopes;
@@ -302,10 +154,8 @@ class AccessTokenResponse extends OAuth2Response {
 
   DateTime? get expirationDate {
     DateTime? expDt;
-
     if (isValid() && respMap.containsKey('expiration_date')) {
-      expDt = respMap['expiration_date'];
-      // expDt = respMap['resp_ts'].add(Duration(seconds: expiresIn!));
+      expDt = DateTime.fromMillisecondsSinceEpoch(respMap['expiration_date']);
     }
 
     return expDt;
