e503516e238bc1eddf7223d03f28d52f43003dc2
okrad <enrico.triolo@gmail.com>
2021-09-28 22:18:42 +0200

    Handle param uri encoding

--- test/oauth2_client_test.dart
+++ test/oauth2_client_test.dart
@@ -389,7 +389,7 @@ void main() {
             containsPair('response_type', 'code'),
             containsPair('client_id', clientId),
             containsPair('redirect_uri', redirectUri),
-            containsPair('scope', scopes.join(' ')),
+            containsPair('scope', scopes.join('+')),
           ));
     });
 
@@ -408,7 +408,7 @@ void main() {
             containsPair('response_type', 'code'),
             containsPair('client_id', clientId),
             containsPair('redirect_uri', redirectUri),
-            containsPair('scope', scopes.join(' ')),
+            containsPair('scope', scopes.join('+')),
             containsPair('state', state),
           ));
     });
@@ -430,7 +430,7 @@ void main() {
             containsPair('response_type', 'code'),
             containsPair('client_id', clientId),
             containsPair('redirect_uri', redirectUri),
-            containsPair('scope', scopes.join(' ')),
+            containsPair('scope', scopes.join('+')),
             containsPair('state', state),
             containsPair('code_challenge', codeChallenge),
             containsPair('code_challenge_method', 'S256'),
