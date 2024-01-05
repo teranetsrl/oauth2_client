53f740ade118256352fde0bca017ff68ea1ecb27
dlatikay <latikaynen@loidl-consulting.at>
2024-01-05 20:26:42 +0100

    #111 Addressed review findings

--- lib/src/io_web_auth.dart
+++ lib/src/io_web_auth.dart
@@ -12,10 +12,12 @@ class IoWebAuth implements BaseWebAuth {
       required String redirectUrl,
       Map<String, dynamic>? opts}) async {
     final preferEphemeral = (opts?['preferEphemeral'] == true);
+    final intentFlags = preferEphemeral ? ephemeralIntentFlags : defaultIntentFlags;
 
     return await FlutterWebAuth2.authenticate(
-        callbackUrlScheme: callbackUrlScheme,
-        url: url,
-        options: FlutterWebAuth2Options(preferEphemeral: preferEphemeral, intentFlags: ephemeralIntentFlags));
+      callbackUrlScheme: callbackUrlScheme,
+      url: url,
+      options: FlutterWebAuth2Options(preferEphemeral: preferEphemeral, intentFlags: intentFlags)
+    );
   }
 }
